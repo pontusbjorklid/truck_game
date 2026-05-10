1fa169cf126cfe855a7fb73a612fba83a7385c5c
Pontus Björklid <pontus.bjorklid@scania.com>
2026-05-10 19:48:44 +0200

    Button animation

--- src/test.py
+++ src/test.py
@@ -5,23 +5,36 @@ import os
 
 class Button:
     def __init__(self, text, width, height, pos):
+        # Core attributes
+        self.pressed = False
+        
         # top rectangle
         self.top_rect = pygame.Rect((pos), (width, height))
-        self.top_color = "#475F77" 
+        self.top_color = "#d8b07a" 
 
         # text
-        self.text_surf = font.render(text, True, "#FFFFFF")
+        self.text_surf = font.render(text, True, "#000000")
         self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
 
     def draw(self):
-        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=12)
+        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=8)
         screen.blit(self.text_surf, self.text_rect)
         self.check_click()
 
     def check_click(self):
         mouse_pos = pygame.mouse.get_pos()
         if self.top_rect.collidepoint(mouse_pos):
-            print("overlap)")
+            self.top_color = "#af7231"
+            if pygame.mouse.get_pressed()[0]:
+                self.pressed = True
+            else:
+                if self.pressed == True:
+                    print("click")
+                    self.pressed = False
+        else:
+            self.top_color = "#d8b07a"
+
+
 
 # Initialize Pygame
 pygame.init()
